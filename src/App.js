2eba1635b5fbc65916752234c01d3fbb64fb8e1d
marine <marine@MarinedeiMac.local>
2020-05-14 09:48:04 +0800

    add particlesSetting file

--- src/App.js
+++ src/App.js
@@ -6,32 +6,7 @@ import Logo from './Components/Logo/Logo';
 import UserLogIn from './Components/UserLogin/UserLogIn';
 import ImgLinkForm from './Components/ImgLinkForm/ImgLinkForm';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
-import { Background } from 'tsparticles/dist/Options/Classes/Background/Background';
-
-const particlesSetting = {
-  particles: {
-    number: {
-      value: 80,
-      density: {
-        enable: true,
-        area: 400
-      }
-    },
-    lineLinked: {
-      color: {
-        value: "#B0AAA8" 
-      },
-    }
-  },
-  interactivity: {
-    events: {
-      onHover: {
-        enable: true,
-        mode: "repulse"
-      }
-    }
-  }
-}
+import {ParticlesSetting} from './Components/ParticlesSetting';
 
 class App extends Component{
 
@@ -39,7 +14,7 @@ class App extends Component{
     return (
       <div className="App">
         <Particles className='particlesClass'
-        params={particlesSetting}
+        params={ParticlesSetting}
         />
         <Navigation/>
         <Logo/>
---
+++ src/Components/ParticlesSetting.js
@@ -0,0 +1,25 @@
+export const ParticlesSetting = {
+    particles: {
+      number: {
+        value: 80,
+        density: {
+          enable: true,
+          area: 400
+        }
+      },
+      lineLinked: {
+        color: {
+          value: "#B0AAA8" 
+        },
+      }
+    },
+    interactivity: {
+      events: {
+        onHover: {
+          enable: true,
+          mode: "repulse"
+        }
+      }
+    }
+  }
+
