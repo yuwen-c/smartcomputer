8235351a6398532ba3b23d09ef97f7e6ddd39e8c
marine <marine@MarinedeiMac.local>
2020-06-25 07:48:52 +0800

    delete say hi p tag, delete unecessary div, adjust margin bottom of image

--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -3,30 +3,26 @@ import './FaceRecognition.css'
 
 const FaceRecognition = ({Pimg, PfaceRegion, PfaceRegions}) => {
     return(
-        // flex-column: column layouts with <p> and child <div>
-        <div className='flex flex-column ma mb2'>
-            <p className="f4">Janet Jackson says Hi to you.</p>
-            {/* justify-center: put child <div> in the middle */}
-            <div className='flex justify-center'>
-                <div className='mt2 relative'>
-                    <img id='imgID' className="" width='500px' height='auto'
-                    src={Pimg}  alt=""/>              
-                    {/* one face version:      
-                    PfaceRegion is an style object, pass it directly to style*/}
-                    {/* <div 
-                    className='faceRegionClass' 
-                    style={PfaceRegion}></div> */}
-                    {
-                        PfaceRegions.map((item, index) => {
-                            return (
-                                <div 
-                                key={index} 
-                                className='faceRegionClass' 
-                                style={item}></div>
-                            )
-                        })
-                    }
-                </div>
+          //  {/* justify-center: put child <div> in the middle */}
+        <div className='flex justify-center'>
+            <div className='mt2 mb2 relative'>
+                <img id='imgID' className="" width='500px' height='auto'
+                src={Pimg}  alt=""/>              
+                {/* one face version:      
+                PfaceRegion is an style object, pass it directly to style*/}
+                {/* <div 
+                className='faceRegionClass' 
+                style={PfaceRegion}></div> */}
+                {
+                    PfaceRegions.map((item, index) => {
+                        return (
+                            <div 
+                            key={index} 
+                            className='faceRegionClass' 
+                            style={item}></div>
+                        )
+                    })
+                }
             </div>
         </div>
     )    
