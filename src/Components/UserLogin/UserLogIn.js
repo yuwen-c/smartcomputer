bcb32615dd30e406edad4ef02559d321e2f10cc3
yuwen <yuwen.azulejos@gmail.com>
2022-09-07 10:57:19 +0800

    chore: center title; upgrade serve

--- src/Components/UserLogin/UserLogIn.js
+++ src/Components/UserLogin/UserLogIn.js
@@ -1,17 +1,12 @@
-import React from 'react';
+import React from "react";
 
-const UserLogIn = ({user}) => {
-    return (
-        <article className="mw8 pt4 ph1">
-            <div className="f3 fw5">
-                {user.name}, your current entries is...
-            </div>
-            <div className="f3 fw8 pt1 pb2">
-                #{user.entries}
-            </div>
-        </article>
-
-    )
-}
+const UserLogIn = ({ user }) => {
+  return (
+    <article className="mw8 pt4 ph1 center">
+      <div className="f3 fw5">{user.name}, your current entries is...</div>
+      <div className="f3 fw8 pt1 pb2">#{user.entries}</div>
+    </article>
+  );
+};
 
 export default UserLogIn;
